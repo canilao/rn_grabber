f8a4527d3649ff80471e190fd4a202b91d13a668
Chris B. Anilao <canilao1@yahoo.com>
2011-01-30 20:14:20 -0600

    Added code that can now do a search on the idfpr server.

--- rn_grabber/Main.cs
+++ rn_grabber/Main.cs
@@ -1,8 +1,11 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Xml;
+using System.Web;
+using System.Text;
 using HtmlAgilityPack;
 
 namespace LearningMono
@@ -86,9 +89,60 @@ namespace LearningMono
 			return sessionCode;
 		}
 		
-		public static void Main(string[] args)
+		public static void SearchForNurses(string sessionKey, string seachString)
 		{
-			System.Console.WriteLine(GetSessionKey());
+			// POST /dpr/licenselookup/results.asp HTTP/1.1
+			// Host: www.idfpr.com
+			// User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13 (.NET CLR 3.5.30729)
+			// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+			// Accept-Language: en-us,en;q=0.5
+			// Accept-Encoding: gzip,deflate
+			// Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7
+			// Keep-Alive: 115
+			// Connection: keep-alive
+			// Referer: http://www.idfpr.com/dpr/licenselookup/default.asp
+			// Cookie: ASPSESSIONIDCABRCRTC=EEJNDGGBAMHANPFBMKKPOBGK
+			// Content-Type: application/x-www-form-urlencoded
+			// Content-Length: 88
+			
+			// TYPE=NAME&pro_cde=0041&lnme=Anilao&checkbox=on&finit=&rowcount=&submit1.x=32&submit1.y=4
+			
+			HttpWebRequest myReq = (HttpWebRequest) WebRequest.Create("http://www.idfpr.com/dpr/licenselookup/results.asp");
+			
+			string postData = "TYPE=NAME&pro_cde=0041&lnme=Anilao&checkbox=on&finit=&rowcount=5000&submit1.x=43&submit1.y=8";
+			byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+			
+			myReq.Method = "POST";
+			myReq.ContentType = "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.13) Gecko/20101206 Ubuntu/10.10 (maverick) Firefox/3.6.13";
+			myReq.Headers.Add("Accept-Language", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+			myReq.Headers.Add("Accept-Encoding", "gzip,deflate");
+			myReq.Headers.Add("Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.7");
+			myReq.Headers.Add("Keep-Alive", "115");
+			myReq.KeepAlive = true;
+			myReq.Referer = "http://www.idfpr.com/dpr/licenselookup/default.asp";
+			myReq.CookieContainer = new CookieContainer();
+			Cookie c = new Cookie("ASPSESSIONIDCABRCRTC", sessionKey, "/", "http://www.idfpr.com");
+			myReq.CookieContainer.Add(c);
+			myReq.ContentType = "application/x-www-form-urlencoded";
+			myReq.ContentLength = byteArray.Length;
+			
+			Stream dataStream = myReq.GetRequestStream();
+			dataStream.Write(byteArray, 0, byteArray.Length);
+			dataStream.Close();
+			
+			WebResponse response = myReq.GetResponse();
+			
+			Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+			
+			dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            Console.WriteLine(responseFromServer);
+		}
+		
+		public static void Main(string[] args)
+		{		
+			SearchForNurses(GetSessionKey(), "anilao");
 		}
 	}
 }
